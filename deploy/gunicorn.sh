545337fad9c0c127ee60c6e130c5091f61c9a954
Bob Briski <bbriski@raybeam.com>
2019-07-26 14:38:22 -0700

    Gunicorn requirements / deploy

--- deploy/gunicorn.sh
+++ deploy/gunicorn.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # Navigate to the backend directory
 cd "${DIR}/.."
 # Activate virtualenv
-. ~/voting-venv/bin/activate
+. ~/venv/bin/activate
 # Start Django with Gunicorn
 gunicorn --bind 127.0.0.1:8000 -w 4 --access-logfile - --error-logfile - wsgi
