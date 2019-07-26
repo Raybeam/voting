981c0b8b9368e1d9baf8cc23f8c60428eafe1438
Bob Briski <bbriski@raybeam.com>
2019-07-26 14:04:15 -0700

    try launch script

---
+++ deploy/gunicorn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Find out the location of the script, not the working directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+# Navigate to the backend directory
+cd "${DIR}/.."
+# Activate virtualenv
+. ~/voting-venv/bin/activate
+# Start Django with Gunicorn
+gunicorn --bind 127.0.0.1:8000 -w 4 --access-logfile - --error-logfile - voting.wsgi
